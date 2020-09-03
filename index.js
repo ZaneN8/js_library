87db11efb75c4fae76ad81a22abf8c1138ef99b2
ZaneN8 <zanelnewsom@gmail.com>
2020-09-03 14:55:33 -0600

    Starting project

---
+++ index.js
@@ -0,0 +1,14 @@
+let state = {
+  pageHeader: "The Library",
+  book: book,
+};
+
+function renderBook() {}
+
+function addBook() {}
+function updateBook() {}
+
+function deleteBook() {}
+function render() {}
+
+render();
